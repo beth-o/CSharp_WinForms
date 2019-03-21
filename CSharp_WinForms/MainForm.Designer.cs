6e2003e44ee7be4f32f6548eeef26c4bc44584b9
Beth Marlatt <beth.marlatt@gmail.com>
2019-03-20 17:55:59 -0700

    updated code

--- CSharp_WinForms/MainForm.Designer.cs
+++ CSharp_WinForms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSharp_WinForms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnMyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnMyButton
+            // 
+            this.BtnMyButton.Location = new System.Drawing.Point(77, 54);
+            this.BtnMyButton.Name = "BtnMyButton";
+            this.BtnMyButton.Size = new System.Drawing.Size(75, 23);
+            this.BtnMyButton.TabIndex = 0;
+            this.BtnMyButton.Text = "My Button";
+            this.BtnMyButton.UseVisualStyleBackColor = true;
+            this.BtnMyButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.BtnMyButton);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnMyButton;
     }
 }
 
--- CSharp_WinForms/MainForm.cs
+++ CSharp_WinForms/MainForm.cs
@@ -10,11 +10,15 @@ using System.Windows.Forms;
 
 namespace CSharp_WinForms
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void BtnMyButton_Click(object sender, EventArgs e) {
+
         }
     }
 }
